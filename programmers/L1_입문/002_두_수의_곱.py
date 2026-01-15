aee788eae9bbf3af223cd6d4db42b5cb41b3d562
muaSeol <66067036+muaSeol@users.noreply.github.com>
2026-01-15 18:38:04 +0900

    [프로그래머스] L1 - 두 수의 곱 (#2)

---
+++ programmers/L1_입문/002_두_수의_곱.py
@@ -0,0 +1,10 @@
+# 두 수의 곱
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120804
+# 알고리즘: 기본연산
+# 작성자: ㅅㅅㅅ
+# 작성일: 2026-01-15
+
+def solution(num1, num2):
+    answer = 0
+    return num1 * num2
