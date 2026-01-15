4ad5beb8c9d2c0a918f33b87cab8e2387f6e0e71
muaSeol <66067036+muaSeol@users.noreply.github.com>
2026-01-15 19:33:13 +0900

    [프로그래머스] L2 - 자릿수 더하기 (#226)

--- programmers/L2_기초/226_자릿수_더하기.py
+++ programmers/L2_기초/226_자릿수_더하기.py
@@ -2,11 +2,11 @@
 # 프로그래머스 L2 (기초)
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/12931
 # 알고리즘: 문자열, 반복문
-# 작성자: 학생
-# 작성일: 2026. 01. 15. 19:32:20
+# 작성자: dd
+# 작성일: 2026. 01. 15. 19:33:12
 
 def solution(n):
     answer = 0
 
    
-    return sum(int(i) for i in str(n))
+    return sum(int(i) for i in str(n)) #ddd
