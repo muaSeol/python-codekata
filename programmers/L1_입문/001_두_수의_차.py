81f326e136239fb308fe350e5ba9825587292e66
muaSeol <66067036+muaSeol@users.noreply.github.com>
2026-01-15 18:37:39 +0900

    [프로그래머스] L1 - 두 수의 차 (#1)

---
+++ programmers/L1_입문/001_두_수의_차.py
@@ -0,0 +1,10 @@
+# 두 수의 차
+# 프로그래머스 L1 (입문)
+# 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120803
+# 알고리즘: 기본연산
+# 작성자: ㅅㅅㅅ
+# 작성일: 2026-01-15
+
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
